92d129642d406be46eed049849e7ceb342565728
TheRealOkone <tnoko@yandex.ru>
2021-07-18 18:46:42 +0300

    fix2

--- src/main/java/controller/PController.java
+++ src/main/java/controller/PController.java
@@ -6,6 +6,7 @@ import interface_.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import ru.parse.Parser;
 @Controller
 public class PController {
@@ -22,11 +23,14 @@ public class PController {
     }
 
     @RequestMapping(value = "/g1", method = RequestMethod.GET)
+    @ResponseBody
     public byte[] gone() {
+        System.out.println("asddd31222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222dddddddd\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
         return new byte[160000];
     }
 
     @RequestMapping(value = "/g2", method = RequestMethod.GET)
+    @ResponseBody
     public String gtwo() {
         return "cringe";
     }
